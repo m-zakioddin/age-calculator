713a16647a6e99a9175067c1af3e3f95fb4c4447
“Muhammed <mzakioddin@entrata.com>
2026-03-31 12:05:54 +0530

    ERROR pushed

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let userInput = document.getElementById ("date");
 userInput.max=new Date().toISOString().split("T")[0];
 let result = document.getElementById("result");
 function calculateAge(){
+
+    return 20;
+    
     let birthDate=new Date(userInput.value);
 
     let d1=birthDate.getDate();
